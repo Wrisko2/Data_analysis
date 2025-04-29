a4c80007e4b2bb90c153220e2f3864b911e44bae
Wrisko2 <staslebedenko68@gmail.com>
2025-04-29 22:02:39 +0300

    Update medical_data_visualizer.py

--- medical/medical_data_visualizer.py
+++ medical/medical_data_visualizer.py
@@ -10,36 +10,35 @@ df = pd.read_csv('medical_examination.csv')
 # Add 'overweight' column
 df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2 > 25).astype(int)
 
-# Normalize data by making 0 always good and 1 always bad. If the value of 'cholestorol' or 'gluc' is 1,
-# make the value 0. If the value is more than 1, make the value 1.
+# Нормалізуємо дані, зробивши 0 завжди хорошим, а 1 завжди поганим. Якщо значення «cholestorol» або «gluc» дорівнює 1,
+# зробити значення 0. Якщо значення більше 1, зробити значення 1.
 df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
 df['gluc'] = (df['gluc'] > 1).astype(int)
 
 
 # Draw Categorical Plot
 def draw_cat_plot():
-    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke',
+    # Створіть DataFrame для діаграми categoria за допомогою `pd.melt`, використовуючи лише значення з 'cholesterol', 'gluc', 'smoke',
     # 'alco', 'active', and 'overweight'.
     df_cat = pd.melt(df, id_vars=['cardio'],
                      value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename
-    # one of the collumns for the catplot to work correctly.
+    # Групуємо та переформатовуємо дані, щоб розділити їх за «cardio»
     df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index()
     df_cat = df_cat.rename(columns={0: 'total'})
 
-    # Draw the catplot with 'sns.catplot()'
+    # Малюємо catplot за допомогою "sns.catplot()"
     graph = sns.catplot(data=df_cat, kind="bar", x="variable", y="total", hue="value", col="cardio")
     fig = graph.fig
 
-    # Do not modify the next two lines
+    # Не змінюємо наступні два рядки
     fig.savefig('catplot.png')
     return fig
 
 
 # Draw Heat Map
 def draw_heat_map():
-    # Clean the data
+    # Очищаємо дату
     df_heat = df[(df['ap_lo'] <= df['ap_hi']) &
                  (df['height'] >= df['height'].quantile(0.025)) &
                  (df['height'] <= df['height'].quantile(0.975)) &
@@ -47,19 +46,19 @@ def draw_heat_map():
                  (df['weight'] <= df['weight'].quantile(0.975))
                  ]
 
-    # Calculate the correlation matrix
+    # Обчислюємо кореляційну матрицю
     corr = df_heat.corr()
 
-    # Generate a mask for the upper triangle
+    # Генеруємл маску для верхнього трикутника
     mask = np.triu(np.ones_like(corr, dtype=bool))
 
-    # Set up the matplotlib figure
+    # Налаштовуємо графік
     fig, ax = plt.subplots(figsize=(16, 9))
 
-    # Draw the heatmap with 'sns.heatmap()'
+    # Малюємо heatmap за допомогою "sns.heatmap()"
     sns.heatmap(corr, mask=mask, square=True, linewidths=0.5, annot=True, fmt="0.1f")
 
-    # Do not modify the next two lines
+    # Не змінюємо наступні два рядки
     fig.savefig('heatmap.png')
     return fig
 
